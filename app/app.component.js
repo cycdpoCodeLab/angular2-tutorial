4b8b29a36273b4f509d153ec5c43238a382b887c
cycjimmy <cycjimmy@gmail.com>
2016-08-18 16:19:33 +0800

    hero 3
    主从结构

--- app/app.component.js
+++ app/app.component.js
@@ -21,23 +21,34 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
+//模拟英雄
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //Component 是一个装饰器函数，它接受一个元数据对象作为参数。 通过给这个函数加上@前缀，并使用metadata对象调用它，可以把这个“函数调用”加到组件类的紧上方。
 //@Component 是一个装饰器，它把元数据关联到组件类上。这些元数据会告诉Angular如何创建和使用组件类。
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        //Hero类中的个体hero
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
             //template 字段指定了此组件的模板。 它用一种增强的HTML形式写成，用来告诉 Angular 如何渲染此组件的视图。可用多行模板注意符号不同
             //[(ngModel)]="hero.name" 实现双向数据绑定
-            template: "                                   \n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<div><label>id:</label>{{hero.id}}</div>\n\t\t<div>\n\t\t\t<label>name:</label>\n\t\t\t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n\t\t</div>\n\t\t"
+            template: "\n        <h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t\t<li *ngFor=\"let hero of heroes\" \n\t\t\t[class.selected]=\"hero === selectedHero\"\n\t\t\t(click)=\"onSelect(hero)\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n\t\t</ul>\n\t\t\n\t\t<div *ngIf=\"selectedHero\">\n\t\t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t\t<div>\n\t\t\t\t<label>name: </label>\n\t\t\t\t<input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\">\n\t\t\t</div>\n\t\t</div>\n\t\t",
+            styles: ["\n\t\t.selected {\n\t\t\tbackground-color: #CFD8DC !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes {\n\t\t\tmargin: 0 0 2em 0;\n\t\t\tlist-style-type: none;\n\t\t\tpadding: 0;\n\t\t\twidth: 15em;\n\t\t}\n\t\t.heroes li {\n\t\t\tcursor: pointer;\n\t\t\tposition: relative;\n\t\t\tleft: 0;\n\t\t\tbackground-color: #EEE;\n\t\t\tmargin: .5em;\n\t\t\tpadding: .3em 0;\n\t\t\theight: 1.6em;\n\t\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\t\tbackground-color: #BBD8DC !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\t\tcolor: #607D8B;\n\t\t\tbackground-color: #DDD;\n\t\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t\tposition: relative;\n\t\t\ttop: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t\tdisplay: inline-block;\n\t\t\tfont-size: small;\n\t\t\tcolor: white;\n\t\t\tpadding: 0.8em 0.7em 0 0.7em;\n\t\t\tbackground-color: #607D8B;\n\t\t\tline-height: 1em;\n\t\t\tposition: relative;\n\t\t\tleft: -1px;\n\t\t\ttop: -4px;\n\t\t\theight: 1.8em;\n\t\t\tmargin-right: .8em;\n\t\t\tborder-radius: 4px 0 0 4px;\n\t\t}\n\t"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -16,6 +16,19 @@ export class Hero {
 	name: string;
 }
 
+//模拟英雄
+const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+	{ id: 12, name: 'Narco' },
+	{ id: 13, name: 'Bombasto' },
+	{ id: 14, name: 'Celeritas' },
+	{ id: 15, name: 'Magneta' },
+	{ id: 16, name: 'RubberMan' },
+	{ id: 17, name: 'Dynama' },
+	{ id: 18, name: 'Dr IQ' },
+	{ id: 19, name: 'Magma' },
+	{ id: 20, name: 'Tornado' }
+];
 
 //Component 是一个装饰器函数，它接受一个元数据对象作为参数。 通过给这个函数加上@前缀，并使用metadata对象调用它，可以把这个“函数调用”加到组件类的紧上方。
 //@Component 是一个装饰器，它把元数据关联到组件类上。这些元数据会告诉Angular如何创建和使用组件类。
@@ -24,27 +37,85 @@ export class Hero {
 
 	//template 字段指定了此组件的模板。 它用一种增强的HTML形式写成，用来告诉 Angular 如何渲染此组件的视图。可用多行模板注意符号不同
 	//[(ngModel)]="hero.name" 实现双向数据绑定
-	template: `                                   
-		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id:</label>{{hero.id}}</div>
-		<div>
-			<label>name:</label>
-			<input [(ngModel)]="hero.name" placeholder="name">
+	template: `
+        <h1>{{title}}</h1>
+		<h2>My Heroes</h2>
+		<ul class="heroes">
+			<li *ngFor="let hero of heroes" 
+			[class.selected]="hero === selectedHero"
+			(click)="onSelect(hero)">
+				<span class="badge">{{hero.id}}</span> {{hero.name}}
+			</li>
+		</ul>
+		
+		<div *ngIf="selectedHero">
+			<h2>{{selectedHero.name}} details!</h2>
+				<div><label>id: </label>{{selectedHero.id}}</div>
+			<div>
+				<label>name: </label>
+				<input [(ngModel)]="selectedHero.name" placeholder="name">
+			</div>
 		</div>
-		`
+		`,
+	styles:[`
+		.selected {
+			background-color: #CFD8DC !important;
+			color: white;
+		}
+		.heroes {
+			margin: 0 0 2em 0;
+			list-style-type: none;
+			padding: 0;
+			width: 15em;
+		}
+		.heroes li {
+			cursor: pointer;
+			position: relative;
+			left: 0;
+			background-color: #EEE;
+			margin: .5em;
+			padding: .3em 0;
+			height: 1.6em;
+			border-radius: 4px;
+		}
+		.heroes li.selected:hover {
+			background-color: #BBD8DC !important;
+			color: white;
+		}
+		.heroes li:hover {
+			color: #607D8B;
+			background-color: #DDD;
+			left: .1em;
+		}
+		.heroes .text {
+			position: relative;
+			top: -3px;
+		}
+		.heroes .badge {
+			display: inline-block;
+			font-size: small;
+			color: white;
+			padding: 0.8em 0.7em 0 0.7em;
+			background-color: #607D8B;
+			line-height: 1em;
+			position: relative;
+			left: -1px;
+			top: -4px;
+			height: 1.8em;
+			margin-right: .8em;
+			border-radius: 4px 0 0 4px;
+		}
+	`]
 })
-
 
 //一个 组件类 来通过它的模板控制一个视图的外观和行为。
 export class AppComponent {
 	title = 'Tour of Heroes';
 
-	//Hero类中的个体hero
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	public heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 
